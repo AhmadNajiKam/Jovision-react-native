2e9de00584acdf6cea34c4320f21bb5038f533bc
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-27 14:35:03 +0300

    Make code more modular to re-use in next tasks

--- Tasks/Task16.js
+++ Tasks/Task16.js
@@ -1,3 +1,4 @@
+'use strict'
 import { useState } from 'react';
 import { Platform, StyleSheet, View, Text, Button, ViewComponent } from 'react-native';
 const Task16 = () =>{
@@ -6,19 +7,24 @@ const Task16 = () =>{
     function ShowMyName(){
         setHidden(!Hidden);
     }
-    return (<>
-<Button
-  onPress={ShowMyName}
-  title="Show"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>
-<View>
-    {!Hidden ? <Text style= {styles.headerText}>{name}</Text>:null};
-</View>
- </>
+    return (<ShowNameButton press={ShowMyName} state={Hidden} name={name}></ShowNameButton>
     );
 } 
+const ShowNameButton = (pressFunction) =>{
+    
+    return (<>
+        <Button
+          onPress={pressFunction.press}
+          title="Show"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <View>
+            {!(pressFunction.state) ? <Text style= {styles.headerText}>{pressFunction.name}</Text>:null};
+        </View>
+         </>
+            );
+}
 const styles = StyleSheet.create({
     headerText: {
         fontSize: 20,
