2c5caee8e66f02c116965b5aaebfbf44bab6be56
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-27 14:51:24 +0300

    Split files to make them more modular

---
+++ Components/ShowNameButtonCom.js
@@ -0,0 +1,26 @@
+import { Platform, StyleSheet, View, Text, Button, ViewComponent } from 'react-native';
+
+const ShowNameButton = (pressFunction) =>{
+    
+    return (<>
+        <Button
+          onPress={pressFunction.press}
+          title="Show"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <View>
+            {!(pressFunction.state) ? <Text style= {styles.headerText}>{pressFunction.name}</Text>:null};
+        </View>
+         </>
+            );
+}
+const styles = StyleSheet.create({
+    headerText: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+        fontWeight: "bold",
+        backgroundColor:"white"}
+})
+export default ShowNameButton;
--- Tasks/Task16.js
+++ Tasks/Task16.js
@@ -1,6 +1,6 @@
 'use strict'
 import { useState } from 'react';
-import { Platform, StyleSheet, View, Text, Button, ViewComponent } from 'react-native';
+import ShowNameButton from '../Components/ShowNameButtonCom';
 const Task16 = () =>{
     const [Hidden,setHidden] = useState(true);
     const name = "ahmad";
@@ -10,28 +10,6 @@ const Task16 = () =>{
     return (<ShowNameButton press={ShowMyName} state={Hidden} name={name}></ShowNameButton>
     );
 } 
-const ShowNameButton = (pressFunction) =>{
-    
-    return (<>
-        <Button
-          onPress={pressFunction.press}
-          title="Show"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <View>
-            {!(pressFunction.state) ? <Text style= {styles.headerText}>{pressFunction.name}</Text>:null};
-        </View>
-         </>
-            );
-}
-const styles = StyleSheet.create({
-    headerText: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10,
-        fontWeight: "bold",
-        backgroundColor:"white"}
-})
+
 
 export default Task16;
